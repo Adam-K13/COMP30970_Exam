17774af641b55181918981a57c23211331582805
Adam Kearns <adamjkearns13@gmail.com>
2025-12-17 13:23:13 +0000

    added files for question A

---
+++ Problem_A_Prime_Reduction.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while(true) {
+        int input_num;
+        cin >> input_num;
+        if(input_num == 4) {
+            break;
+        }
+
+        
+
+
+
+
+
+    }
+
+    return 0;
+}
